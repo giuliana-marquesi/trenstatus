548e9c15e06dbc7b63c8c3a6d9e6a4291e81e104
Giuliana Marquesi <giugianam@gmail.com>
2017-05-17 17:58:01 -0300

    twitter-heroku: feita a logica para incluir no bd, adicionado no index

--- twitter-heroku/index.js
+++ twitter-heroku/index.js
@@ -67,13 +67,21 @@ var queries = [
   "@Linha12_CPTM",
 ];
 
-var lines = [
-  "Azul",
-  "Vermelha",
-  "Amarela",
-  "Lilas",
-  "Verde"
-];
+var linha = {
+  azul: [/azul/g,/linha1/g, /l1/g],
+  verde: [/verde/g, /linha2/g, /l2/g,],
+  vermelha: [/vermelha/g, /linha3/g, /l3/g,],
+  amarela: [/amarela/g, /linha4/g, /l4/g,],
+  lilas: [/lilas/g, /linha5/g, /l5/g],
+  rubi: [/rubi/g, /linha7/g, /l7/g],
+  diamante: [/diamante/g, /linha8/g, /l8/g],
+  esmeralda: [/esmeralda/g, /linha9/g, /l9/g],
+  turquesa: [/turquesa/g, /linha10/g, /l10/g],
+  coral: [/coral/g, /linha11/g, /l11/g],
+  safira: [/safira/g, /linha12/g, /l12/g],
+  prata: [/prata/g, /linha15/g, /l15/g],
+};
+
 var queryString = queries.join(", ");
 
 var insertIndex = 0;
@@ -93,10 +101,10 @@ var client = new Twitter({
 client.stream('statuses/filter', {track: queryString, language: "pt"}, function(stream){
   stream.on('data', function(tweet) {
     var mText = tweet.text;
-    console.log(mText);
     if(mQueue.length < QUEUE_SIZE) {
-      mText = selectTrainLine(mText);
+      mText = arranjaTwittesPelaLinha(mText);
       mQueue.push(mText);
+      console.log(mText);
     } else {
       mQueue[insertIndex] = mText;
       insertIndex = (insertIndex + 1)%mQueue.length;
@@ -109,12 +117,32 @@ client.stream('statuses/filter', {track: queryString, language: "pt"}, function(
   });
 });
 
-function selectTrainLine(mText) {
-  if(mText){
-    return mText;
-  }
-  return mText;
+function arranjaTwittesPelaLinha(tuite) {
+  tuite = tuite.toLowerCase();
+  for(var nome_linha in linha) {
+    valores = linha[nome_linha];
+    for(var i in valores){
+      var valor = valores[i];
+      if(tuite.match(valor)){
+        var tuiteLimpo = limpaTuite(tuite);
+        return tuiteLimpo;
+      }
+    };
+  };
 };
+
+function limpaTuite(tuite) {
+  var tuiteLimpo = tuite.replace(/RT /g, "");
+    tuiteLimpo = tuiteLimpo.replace(/["{}<>().!,;|\-]/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/[#@]\S+/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/http(s?):\/\/\S+/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/b\/c/g, "because");
+    tuiteLimpo = tuiteLimpo.replace(/([a-zA-Z]+)\/([a-zA-Z]+)/g, "$1 $2");
+    tuiteLimpo = tuiteLimpo.replace(/\S+…/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/\s+/g, " ");
+    tuiteLimpo = tuiteLimpo.trim();
+    return tuiteLimpo;
+}
 
 app.get('/AFT', function(req, res) {
   res.send(mQueue[popIndex]);
--- twitter-heroku/teste-bd.js
+++ twitter-heroku/teste-bd.js
@@ -7,6 +7,54 @@ var texto = [
   "Devido à greve, não há previsão de normalização da #L3 hoje. Linha segue paralisada. @grupo_diario #MetroSP | 17h04,",
   "RT @Linha3vermelha: Devido à greve, não há previsão de normalização da #L3 hoje. Linha segue paralisada. @grupo_diario #MetroSP | 17h04,",
   "Atualização [18:32] L1 Saúde x Luz (exceto Sé) L2 V. Madalena x A. Rosa L3 paralisada L4 normal L5 normal L15 paralisada #metrosp,",
+  "Unhappily submitting to the Machine destroys your soul not all at once but over time,",
+  "@metrosp_oficial: 28/04/2017 10:18: #metrosp : Linha 2-Verde: Operação Parcial. Mais informações em,",
+  "Acompanhe todas as atualizações da circulação da #CPTM e #MetroSP através do link:,",
+  "@CPTM_oficial A linha 9 esmeralda ta funcionando parcialmente?pessoal que foi até santo amaro tá reclamando que não tem trem...,",
+  "@CPTM_oficial Bom dia....linha coral da Cptm funciona normal???,",
+  "@scorses @cloowks olá, Victor! Por aqui vc poderá ter uma ideia 😁,",
+  "@CPTM_oficial Vão abrir a estação jaragua?,",
+  "#cptm #linha10 #turquesa em Mauá estação de trem fechada,",
+  "@CPTM_oficial Bom dia, a linha 12 safira ainda está paralisada?,",
+  "#CPTM - 10:22:01 - Linha 9-Esmeralda - Operação Normal,",
+  "@Direto_da_CPTM: #CPTM - 10:22:01 - Linha 9-Esmeralda - Operação Normal,",
+  "ATUALIZAÇÃO: L1 Luz x Paraíso L2 Clínicas x Paraíso L3 Paralisada L4 Normal L5 Normal #grevegeral #metrosp,",
+  "@Diegoferraz2421: @DiariodaCPTM linha 8 funcionando entre Osasco até a Barra Funda.,",
+  "@Direto_do_Metro: #MetroSP - 10:18:01 - Linha 2-Verde - Operação Parcial - Devido à greve dos Metroviários o Met…,",
+  "@governosp @geraldoalckmin_ @CPTM_oficial @metrosp_oficial PRIVATIZA JÁ!!!,",
+  "@DiariodaCPTM Linha 9 Coral está sem previsões ?,",
+  "@Brasil_de_Fato: SÃO PAULO | Estação de trem @CPTM_oficial na Barra Funda completamente vazia. #BrasilemGreve ht…,",
+  "@Diegoferraz2421: @DiariodaCPTM linha 8 funcionando entre Osasco até a Barra Funda.,",
+  "@CPTM_oficial De póa a luz ainda não tem nada funcionando????,",
+  "@CPTM_oficial A linha 9 esmeralda tá funcionando mesmo a partir da jurubatuba?,",
+  "Em Itaquera só gente de bem querendo ir trabalhar e o vagabundo do metrô ferrando o pessoal. Vergonha @metrosp_oficial,",
+  "@metrosp_oficial N tem graça em sp todo arrega .. ou para ou n para,",
+  "ATUALIZAÇÃO: #L8 com operação parcial entre as estações Osasco e Barra Funda. @grupo_diario #CPTM | 10h24,",
+  "@Linha8Diamante: ATUALIZAÇÃO: #L8 com operação parcial entre as estações Osasco e Barra Funda. @grupo_diario #CPTM | 10h24,",
+  "@CPTM_oficial Sobre a L9, o trem já está partindo da estação Grajaú?,",
+  "@metrosp_oficial: 28/04/2017 10:18: #metrosp : Linha 2-Verde: Operação Parcial. Mais informações em,",
+  "@Linha8Diamante: ATUALIZAÇÃO: #L8 com operação parcial entre as estações Osasco e Barra Funda. @grupo_diario #CPTM | 10h24,",
+  "@metrosp_oficial: 28/04/2017 10:18: #metrosp : Linha 2-Verde: Operação Parcial. Mais informações em,",
+  "Linha 10 - turquesa da @CPTM_oficial indo apenas até o Brás e não até a Luz como foi noticiado. #GreveGeral,",
+  "@cptmnoticiando: O comunicado que pode ser entregue como justificativa está disponível no link . #CPTM,",
+  "@metrosp_oficial Linha azul tem possibilidade de voltat a funcionar normalmente hoje ainda?,",
+  "@CPTM_oficial @sptrans_ Outra empresa que deveriam privatizar! A linha amarela do metro está lá, funcionando e atendendo o cidadão,",
+  "@metrosp_oficial Amanha o metro funcionara normal?,",
+  "@metrosp_oficial bom dia! A estacao vila prudente da linha verde esta funcionando?,",
+  "Linha 8 acabou de abrir a catraca na Barra Funda até Osasco! 4 linhas da @CPTM_oficial começam a funcionar parcialmente,",
+  "#L8 dá #CPTM só começou rodar agora, primeiro trem com destino à #Osasco,",
+  "ATUALIZAÇÃO: #L9 com operação parcial entre as estações Jurubatuba e Pinheiros. @grupo_diario #CPTM | 10h28,",
+  "@Wagner_Reid: #L8 dá #CPTM só começou rodar agora, primeiro trem com destino à #Osasco,",
+  "@Linha9Esmeralda: ATUALIZAÇÃO: #L9 com operação parcial entre as estações Jurubatuba e Pinheiros. @grupo_diario #CPTM | 10h28,",
+  "@CPTM_oficial Bom dia, linha 11 está funcionando de guaianazes até à luz?,",
+  "@Linha9Esmeralda: ATUALIZAÇÃO: #L9 com operação parcial entre as estações Jurubatuba e Pinheiros. @grupo_diario #CPTM | 10h28,",
+  "@UsuariosMetroSP Mudou, está indo só até o Brás! #CPTM,",
+  "#CPTM - A @Linha8_CPTM operação parcial entre Osasco e Palmeiras Barra Funda.,",
+  "#CPTM - 10:30:01 - Linha 7-Rubi - Operação Normal,",
+  "@cptmnoticiando: #CPTM - A @Linha8_CPTM operação parcial entre Osasco e Palmeiras Barra Funda.,",
+  "@Linha4Amarela: ⚠️ #L7 Rubi da #CPTM: Os trens estão circulando entre as estações Luz e Pirituba.,",
+  "Até as 18h provavelmente já vai ter voltado ao normal.,",
+  "Acabei de ler que o @metrosp_oficial está funcionando razoavelmente bem,",
 ];
 
 var linha = {
@@ -29,10 +77,9 @@ var mQueue = [
 ];
 
 for(var i = 0; i < texto.length; i++) {
-  console.log("Texto antes da chamada no for" + texto[i]);
+  console.log("Texto antes da chamada no for " + texto[i]);
   var text = arranjaTwittesPelaLinha(texto[i]);
   if(text) {
-    console.log('tem texto');
     mQueue.push(text);
   }
 }
@@ -40,18 +87,37 @@ for(var i = 0; i < texto.length; i++) {
 console.log(mQueue);
 
 function arranjaTwittesPelaLinha(tuite) {
-  //console.log("texto dentro do selectTrainLine " + mText);
+  //console.log("texto dentro do selectTrainLine " + tuiteLimpo);
   tuite = tuite.toLowerCase();
-  //console.log("texto depois do lowercase" + mText);
-  _.each(linha, function(line) {
-    //console.log("texto no each: " + mText);
-    if(tuite.match(line[0])){
-      console.log("encontrado o termo " + line);
-      return tuite;
-    }
-    else{
-      console.log("ERR:Nao encontrado o termo " + line);
-      return tuite;
-    }
-  });
+  //console.log("texto depois do lowercase" + tuiteLimpo);
+  for(var nome_linha in linha) {
+    console.log(nome_linha);
+    valores = linha[nome_linha];
+    console.log(valores);
+    for(var i in valores){
+      var valor = valores[i];
+      console.log(valor);
+      if(tuite.match(valor)){
+        console.log("encontrada a linha " + nome_linha);
+        var tuiteLimpo = limpaTuite(tuite);
+        return tuiteLimpo;
+      }
+      else{
+        console.log("ERR:Nao encontrado o termo " + valor);
+      }
+    };
+  };
 };
+
+function limpaTuite(tuite) {
+  var tuiteLimpo = tuite.replace(/RT /g, "");
+    tuiteLimpo = tuiteLimpo.replace(/["{}<>().!,;|\-]/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/[#@]\S+/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/http(s?):\/\/\S+/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/b\/c/g, "because");
+    tuiteLimpo = tuiteLimpo.replace(/([a-zA-Z]+)\/([a-zA-Z]+)/g, "$1 $2");
+    tuiteLimpo = tuiteLimpo.replace(/\S+…/g, "");
+    tuiteLimpo = tuiteLimpo.replace(/\s+/g, " ");
+    tuiteLimpo = tuiteLimpo.trim();
+    return tuiteLimpo;
+}
